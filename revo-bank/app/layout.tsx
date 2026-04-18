2983151a476f9cdd2521ee3b118a65a459bbae6e
clarissawi609-source <clarissawi609@gmail.com>
2026-04-18 12:55:58 +0700

    fix prisma config & database connection

---
+++ revo-bank/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({
+  weight: '400',
+  subsets: ["latin"],
+});
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "revo-bank",
+  description: "Trusted bank app Developed by Clarissa Wijaya",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>{children}</body>
+    </html>
+  );
+}
---
+++ revo-bank/app/page.tsx
@@ -0,0 +1,7 @@
+const HomePage = () => {
+  return (<main>
+    <h1> Revo Bank </h1>
+  </main>);
+}
+ 
+export default HomePage;
